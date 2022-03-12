c6f677f8fd4a95d5900ad1624cf7a150c8116639
serhiokote@gmail.com <serhiokote@gmail.com>
2022-03-12 15:27:06 +0000

    dbt test zcamp week 4

--- models/ny_taxi_data_dbt/zcamp_temp.sql
+++ models/ny_taxi_data_dbt/zcamp_temp.sql
@@ -3,7 +3,7 @@
 
 
 SELECT *
-FROM {{ source('data_needed', 'fhv_tripdata')  }} --`dtc-de-course-339411.trips_data_all.fhv_tripdata` 
+FROM {{ source('trips_data_all_colours', 'fhv_tripdata')  }} --`dtc-de-course-339411.trips_data_all.fhv_tripdata` 
 WHERE DATE(pickup_datetime) >= "2019-05-24"
     and DATE(pickup_datetime) < "2019-05-25"
     and PULocationID < 20
